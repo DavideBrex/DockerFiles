b793435cea25f8cba0598c1434f21d461d71c47e
Davide Bressan <48018596+DavideBrex@users.noreply.github.com>
2021-07-21 17:58:33 +0200

    Add files via upload

---
+++ TEs-Analyser-container/install_packages_bioc.R
@@ -0,0 +1,17 @@
+# bioconductor
+BiocManager::install(c(
+        'org.Mm.eg.db',
+        'org.Hs.eg.db',
+        'topGO',
+        'GO.db',
+        'GOSemSim',
+        'reactome.db',
+        'DESeq2',
+        'clusterProfiler',
+        'pcaExplorer',
+        'ReactomePA',
+        'IHW',
+        'apeglm',
+        'tximport',
+        'rhdf5',
+        'GenomicFeatures'))
---
+++ TEs-Analyser-container/install_packages_cranDev.R
@@ -0,0 +1,19 @@
+# cran
+install.packages(c(
+        "data.table",
+        "RColorBrewer",
+        "devtools",
+        "tidyverse",
+        "dplyr",
+        "ggrepel",
+        "tibble",
+        "Rcpp",
+        "openxlsx",
+        "msigdbr",
+        "ggforce",
+        "ggplot2"),
+        repos="https://cloud.r-project.org/")
+
+# devtools
+devtools::install_github(c(
+    "VPetukhov/ggrastr"))
